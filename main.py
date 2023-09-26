cf326fee280636fba8c0f285c6fe283da1ec4456
Vavilov <red_white_94@mail.rugit config --global --listgit config --global user.name Vavilov>
2023-09-26 22:43:38 +0300

    dorabotka

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import os
 
-
-def merge_files(file_paths, result_file_path):
-    files_with_line_counts = [(sum(1 for line in open(file_paths, 'r')), file_path) for file_path in file_paths]
+def merge_files(file_paths, result_file):
+    files_with_line_counts = [(sum(1 for line in open(file_path, 'r')), file_path) for file_path in file_paths]
     sorted_files = sorted(files_with_line_counts, key=lambda x: x[0])
-    with open(result_file_path, 'w') as result_file:
+    with open(result_file, 'w') as result:
         for line_count, file_path in sorted_files:
-            result_file.write(f'File Name: {os.path.basename(file_path)}\n')
-            result_file.write(f'Line Count: {line_count}\n')
+            result.write(f'Имя файла: {os.path.basename(file_path)}\n')
+            result.write(f'Строк в файле: {line_count}\n')
             with open(file_path, 'r') as source_file:
                 for line in source_file:
-                    result_file.write(line)
-                    result_file.write('\n')
-
-
+                    result.write(line)
+                    result.write('\n')
 
 merge_files(['1.txt', '2.txt', '3.txt'], 'result.txt')
-
 
 with open('result.txt', 'r') as f:
     print(f.read())
